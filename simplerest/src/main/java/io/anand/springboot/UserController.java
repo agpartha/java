985c2d3401ca76fb108126491fe4da054dfb7434
Anand Girish B. Parthasarathy <code.ag@outlook.com>
2017-03-01 11:19:46 -0800

    Move to mutable list to allow post and put methods

--- simplerest/src/main/java/io/anand/springboot/UserController.java
+++ simplerest/src/main/java/io/anand/springboot/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +24,9 @@ public class UserController {
 	public User getUser (@PathVariable String name) {
 		return userService.getUser(name);
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics/")
+	public User addUser (@PathVariable String name) {
+		return userService.getUser(name);
+	}
 }
--- simplerest/src/main/java/io/anand/springboot/UserService.java
+++ simplerest/src/main/java/io/anand/springboot/UserService.java
@@ -1,5 +1,6 @@
 package io.anand.springboot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,15 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 	
-	private List<User> users = Arrays.asList(
-			new User ("John", "Elementary", 192838, 145354545),
-			new User ("Ravi", "Middle", 192298, 145312144),
-			new User ("David", "Elementary", 192848, 145354652),
-			new User ("Mary", "Middle", 192858, 145351234),
-			new User ("Vinh", "High School", 192538, 14534567),
-			new User ("Surya", "High School", 193138, 14538613)
-			);
-	
+	private ArrayList<User> users;
+
+    public UserService () {
+    	users = new ArrayList<User> ();
+    	users.add(new User ("John", "Elementary", 192838, 145354545));
+		users.add(new User ("Ravi", "Middle", 192298, 145312144));
+		users.add(new User ("David", "Elementary", 192848, 145354652));
+		users.add(new User ("Mary", "Middle", 192858, 145351234));
+		users.add(new User ("Vinh", "High School", 192538, 14534567));
+		users.add(new User ("Surya", "High School", 193138, 14538613));
+    }
+			
 	public List<User> getUsers () {
 		return users;
 	}
