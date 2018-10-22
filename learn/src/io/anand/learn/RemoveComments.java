2d0af07e7fa82960b4d90fbd2a9c7627f558c764
agpartha <code.ag@outlook.com>
2018-10-22 16:24:34 -0700

    State Machine based handling, works for multiple occirrences of comments in each line. Extra memory limited to configurable size independent of input line size.

--- learn/src/io/anand/learn/RemoveComments.java
+++ learn/src/io/anand/learn/RemoveComments.java
@@ -2,6 +2,7 @@ package io.anand.learn;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class RemoveComments {
     // 1. Detect the comments start,
@@ -14,11 +15,48 @@ public class RemoveComments {
     // Single line comments are handled without any state if we are not already in
     // a multi-line comment.
 
-    static boolean commentStart = false;
-    static boolean blockComment = false;   // /* */ style
-    static String  singleChars  = "//";
-    static String  blockBeginChars = "/*";
-    static String  blockEndChars   = "*/";
+    private enum CommentState  {
+            NOT_IN_COMMENT,
+            MAYBE_COMMENT_START,
+            IN_SINGLE_LINE_COMMENT,
+            IN_MULTI_LINE_COMMENT,
+            MAYBE_COMMENT_END
+    }
+
+    private static CommentState commentState = CommentState.NOT_IN_COMMENT;
+    // Fix our max line length to be 80. Anything more we flush and re-use.
+    private static int lineOutIndex = 0;
+    private static char [] lineOut = new char [80];
+    private static boolean commentStart = false;
+    private static boolean blockComment = false;
+    private static String  singleChars  = "//";
+    private static String  blockBeginChars = "/*";
+    private static String  blockEndChars   = "*/";
+
+    // Fliush any characters in the output buffer.
+    static void flushOutputCharacters () {
+        if (0 < lineOutIndex) {
+            System.out.println(new String(lineOut, 0, lineOutIndex));
+            lineOutIndex = 0;
+        }
+    }
+
+    //
+    // Wrapper to handle the output characters.
+    // This will help to buffer the characters till we hit a new line or
+    // buffer size to flush to conosle.
+    // This helps us to use same buffer size irrespective of line input length.
+    static void addOutputCharacter(char c)
+    {
+        // If we have room to add this character stash it.
+        if (lineOutIndex < (lineOut.length - 1)) {
+            lineOut[lineOutIndex++] = c;
+        }
+        // if we are full or the newly added character is a new line flush it
+        if ((c == '\n') || (lineOutIndex == lineOut.length)) {
+            flushOutputCharacters();
+        }
+    }
 
     // Returns the starting index in the string where the
     // single line comment is found.
@@ -47,11 +85,75 @@ public class RemoveComments {
     // since block comments can start and complete many times.
     // if we find a single comment out of block comment segment, we skip the rest of the line.
     // It
+    static void processCommentBytes (String line, boolean outputEnabled) {
+
+        int length = line.length();
+        int i      = 0;
+        int j      = 0;
+
+        // till we have some characters
+        while (i < length) {
+            switch (commentState) {
+                case NOT_IN_COMMENT:
+                    if (line.charAt(i) == '/')
+                        commentState = CommentState.MAYBE_COMMENT_START;
+                    else
+                        addOutputCharacter(line.charAt(i));
+                    break;
+
+                case MAYBE_COMMENT_START:
+                    if (line.charAt(i) == '/')
+                        commentState = CommentState.IN_SINGLE_LINE_COMMENT;
+                    else if (line.charAt(i) == '*')
+                        commentState = CommentState.IN_MULTI_LINE_COMMENT;
+                    else {
+                        addOutputCharacter(line.charAt(i - 1));
+                        addOutputCharacter(line.charAt(i));
+                        commentState = CommentState.NOT_IN_COMMENT;
+                    }
+                    break;
+
+                case IN_SINGLE_LINE_COMMENT:
+                    if (line.charAt(i) == '\n') {
+                        addOutputCharacter(line.charAt(i));
+                        commentState = CommentState.NOT_IN_COMMENT;
+                    }
+                    break;
+
+                case IN_MULTI_LINE_COMMENT:
+                    if (line.charAt(i) == '*')
+                        commentState = CommentState.MAYBE_COMMENT_END;
+                    break;
+
+                case MAYBE_COMMENT_END:
+                    if (line.charAt(i) == '/')
+                        commentState = CommentState.NOT_IN_COMMENT;
+                    else
+                        commentState = CommentState.IN_MULTI_LINE_COMMENT;
+                    break;
+            }
+            // Always move the character to next one
+            i++;
+        }
+        // End of the line means implicit New line.
+        // Flush any pending line characters.
+        // If in single comment state, reset.
+        if (CommentState.IN_SINGLE_LINE_COMMENT == commentState)
+            commentState = CommentState.NOT_IN_COMMENT;
+        flushOutputCharacters();
+    }
+
+    //
+    // Treat the line to have many possible states
+    // since block comments can start and complete many times.
+    // if we find a single comment out of block comment segment, we skip the rest of the line.
+    // It
     static void processCommentLines (String line, boolean outputEnabled) {
         int length = line.length();
         int i = 0;
         // till we have some characters
         while (i < length) {
+            String testStr3= /*TEST: yes*/"\" \' \" // /*/* \\ */*/";//TEST: very uncool
             String remLine = (String) line.substring(i);
             int slcIndex        = findSingleComment(remLine);
             int mlcStartIndex   = findBlockCommentStart(remLine);
@@ -159,7 +261,8 @@ public class RemoveComments {
         String input;
 
         while (null != (input = br.readLine())) {
-            processCommentLines(input, true);
+            processCommentBytes(input, true);
+//            processCommentLines(input, true);
             /*
             if (false == commentStart) {
                 detectCommentStart(input, true);
