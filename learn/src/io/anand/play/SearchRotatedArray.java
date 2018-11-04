143e7a4a1500967490471aa464c69c50c7ed6fb5
sjayashree <jshree@gmail.com>
2018-11-04 11:20:59 -0800

    Simplify logic for determining which half of the array.

--- learn/src/io/anand/play/SearchRotatedArray.java
+++ learn/src/io/anand/play/SearchRotatedArray.java
@@ -1,5 +1,7 @@
 package io.anand.play;
 
+import java.util.Arrays;
+
 public class SearchRotatedArray {
     //a is sorted array
     private static int binSearchRecurse(int[] a, int key, int start, int end) {
@@ -17,37 +19,38 @@ public class SearchRotatedArray {
         // If the value is at any one of the locations we have access to, just check and be done with it.
         if (a[mid] == key)
             return mid;
+        /*
         if (a[start] == key)
             return start;
         if (a[end] == key)
             return end;
-
+*/
         boolean firstHalfSorted   = a[start] < a[mid];
         boolean secondHalfSorted  = a[mid] < a[end];
 
-        boolean searchSecondHalf;
+        boolean searchFirstHalf;
         if (firstHalfSorted && (key >= a[start]) && (key < a[mid]))
             // If the first half is regular sorted part (start < mid) and our key value is >= start of the section
             // and < middle value -> search start -> mid - 1
-            searchSecondHalf = false;
+            searchFirstHalf = true;
         else if (secondHalfSorted && (key > a[mid]) && (key <= a[end]))
             // If the mid onward is regular sorted part (mid < end) and our key value is > mid of the section
             // and l<= end value -> search mid + 1 -> end
-            searchSecondHalf = true;
+            searchFirstHalf = false;
         else if (!firstHalfSorted)
             // UnSorted first half and have eliminated the possibility of key in second sorted half.
-            searchSecondHalf = false;
+            searchFirstHalf = true;
         else if (!secondHalfSorted)
             // Unsorted second half, and have eliminated the possibility of key in second sorted half.
-            searchSecondHalf = true;
+            searchFirstHalf = false;
         else
             return -1;
 
         // Adjust the next search indices
-        if (searchSecondHalf)
-            return binSearchRecurse(a, key,mid + 1, end);
-        else
+        if (searchFirstHalf)
             return binSearchRecurse(a, key, start, mid -1);
+        else
+            return binSearchRecurse(a, key,mid + 1, end);
     }
 
     //a is sorted array
@@ -69,42 +72,44 @@ public class SearchRotatedArray {
             // If the value is at any one of the locations we have access to, just check and be done with it.
             if (a[mid] == key)
                 return mid;
+            /*
             if (a[start] == key)
                 return start;
             if (a[end] == key)
                 return end;
-
+*/
             boolean firstHalfSorted   = a[start] < a[mid];
             boolean secondHalfSorted  = a[mid] < a[end];
 
-            boolean searchSecondHalf;
-            if (firstHalfSorted && (key >= a[start]) && (key < a[mid]))
-                // If the first half is regular sorted part (start < mid) and our key value is >= start of the section
-                // and < middle value -> search start -> mid - 1
-                searchSecondHalf = false;
-            else if (secondHalfSorted && (key > a[mid]) && (key <= a[end]))
-                // If the mid onward is regular sorted part (mid < end) and our key value is > mid of the section
-                // and l<= end value -> search mid + 1 -> end
-                searchSecondHalf = true;
-            else if (!firstHalfSorted)
-                // UnSorted first half and have eliminated the possibility of key in second sorted half.
-                searchSecondHalf = false;
-            else if (!secondHalfSorted)
-                // Unsorted second half, and have eliminated the possibility of key in second sorted half.
-                searchSecondHalf = true;
-            else
+            boolean searchFirstHalf;
+            if (firstHalfSorted) {
+                if ((key >= a[start]) && (key < a[mid]))
+                    // If the first half is sorted  (start < mid) and our key value is >= start and < middle
+                    searchFirstHalf = true;
+                else
+                    // Sorted, but key is out of range, only chance is second half.
+                    searchFirstHalf = false;
+            } else if (secondHalfSorted)  {
+                    if ((key > a[mid]) && (key <= a[end]))
+                    // If the second half is sorted (mid < end) and our key value is > middle and <= end
+                    searchFirstHalf = false;
+                else
+                    // Sorted, but key is out of range, only chance is first half.
+                    searchFirstHalf = true;
+            } else
+                // Technically not valid since array is sorted atleast in one of the halves.
                 return -1;
 
             // Adjust the next search indices
-            if (searchSecondHalf)
-                start = mid + 1;
-            else
+            if (searchFirstHalf)
                 end = mid - 1;
+            else
+                start = mid + 1;
         }
         return -1;
     }
 
-    private static boolean useRecursion = true;
+    private static boolean useRecursion = false;
     private static int binarySearchRotated (int [] a, int key) {
         if (useRecursion)
             return binSearchRecurse(a, key, 0, a.length - 1);
@@ -113,9 +118,11 @@ public class SearchRotatedArray {
 
     public static void main(String []args){
         int[] v1 = {6, 7, 1, 2, 3, 4, 5};
+        System.out.println("Input: " + Arrays.toString(v1));
         System.out.println("Key(3) found at: "+binarySearchRotated(v1, 3));
         System.out.println("Key(6) found at: "+binarySearchRotated(v1, 6));
         int[] v2 = {4, 5, 6, 1, 2, 3};
+        System.out.println("Input: " + Arrays.toString(v2));
         System.out.println("Key(3) found at: "+binarySearchRotated(v2, 3));
         System.out.println("Key(6) found at: "+binarySearchRotated(v2, 6));
     }
