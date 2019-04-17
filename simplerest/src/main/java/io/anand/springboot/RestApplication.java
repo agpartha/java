c05c21630ae8437146167b7730010628fa2aea0d
Anand Parthasarathy <code.ag@outlook.com>
2019-04-17 15:42:32 -0700

    tabs -> spaces

--- simplerest/src/main/java/io/anand/springboot/RestApplication.java
+++ simplerest/src/main/java/io/anand/springboot/RestApplication.java
@@ -16,51 +16,51 @@ import java.util.jar.Manifest;
 @SpringBootApplication
 public class RestApplication {
 
-	private static String getAppStringFromManifest() throws IOException {
-		String appString;
+    private static String getAppStringFromManifest() throws IOException {
+        String appString;
 
-		Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
-				.getResources("META-INF/MANIFEST.MF");
-		while (resources.hasMoreElements()) {
-			URL manifestUrl = resources.nextElement();
-			Manifest manifest = new Manifest(manifestUrl.openStream());
-			Attributes mainAttributes = manifest.getMainAttributes();
-			String startClass = mainAttributes.getValue("Start-Class");
-			if (null != startClass) {
-				appString = startClass;
-				String buildString = mainAttributes.getValue("build");
-				if (buildString != null)
-					return appString + " - Version: " + buildString;
-				else
-					return appString;
-			}
-		}
-		return "";
-	}
+        Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
+                .getResources("META-INF/MANIFEST.MF");
+        while (resources.hasMoreElements()) {
+            URL manifestUrl = resources.nextElement();
+            Manifest manifest = new Manifest(manifestUrl.openStream());
+            Attributes mainAttributes = manifest.getMainAttributes();
+            String startClass = mainAttributes.getValue("Start-Class");
+            if (null != startClass) {
+                appString = startClass;
+                String buildString = mainAttributes.getValue("build");
+                if (buildString != null)
+                    return appString + " - Version: " + buildString;
+                else
+                    return appString;
+            }
+        }
+        return "";
+    }
 
 
-	private static String getBuildNumberFromProps() throws IOException {
-		String propertiesName = "/build.properties";
+    private static String getBuildNumberFromProps() throws IOException {
+        String propertiesName = "/build.properties";
 
-		InputStream propertiesStream = Main.class
-				.getResourceAsStream(propertiesName);
-		if (propertiesStream != null) {
-			Properties pros = new Properties();
-			pros.load(propertiesStream);
+        InputStream propertiesStream = Main.class
+                .getResourceAsStream(propertiesName);
+        if (propertiesStream != null) {
+            Properties pros = new Properties();
+            pros.load(propertiesStream);
 
-			return pros.getProperty("buildNumber");
-		}
-		return null;
-	}
-	
-	public static void main (String [] args) {
-		try {
-			System.out.println("Starting: " + getAppStringFromManifest());
-			System.out.println("Starting: " + getBuildNumberFromProps());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		SpringApplication.run(RestApplication.class, args);
-	}
+            return pros.getProperty("buildNumber");
+        }
+        return null;
+    }
+
+    public static void main (String [] args) {
+        try {
+            System.out.println("Starting: " + getAppStringFromManifest());
+            System.out.println("Starting: " + getBuildNumberFromProps());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(RestApplication.class, args);
+    }
 
 }
