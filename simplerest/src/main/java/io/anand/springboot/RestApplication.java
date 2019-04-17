cbebc07b44cceee1cbe8d6e7e2434f5c36bc489d
Anand Parthasarathy <code.ag@outlook.com>
2019-04-17 12:46:43 -0700

    Add build number to jar manifest and retrieve it in the main appliation. running in IntelliJ does not get this, move to properties file as a reource to be able to work anywhere

--- simplerest/src/main/java/io/anand/springboot/RestApplication.java
+++ simplerest/src/main/java/io/anand/springboot/RestApplication.java
@@ -3,11 +3,46 @@ package io.anand.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.JarInputStream;
+import java.util.jar.Manifest;
+
 @SpringBootApplication
 public class RestApplication {
+
+	private static String getAppStringFromManifest() throws IOException {
+		String appString;
+
+		Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
+				.getResources("META-INF/MANIFEST.MF");
+		while (resources.hasMoreElements()) {
+			URL manifestUrl = resources.nextElement();
+			Manifest manifest = new Manifest(manifestUrl.openStream());
+			Attributes mainAttributes = manifest.getMainAttributes();
+			String startClass = mainAttributes.getValue("Start-Class");
+			if (null != startClass) {
+				appString = startClass;
+				String buildString = mainAttributes.getValue("build");
+				if (buildString != null)
+					return appString + " - Version: " + buildString;
+				else
+					return appString;
+			}
+		}
+		return "";
+	}
 	
 	public static void main (String [] args) {
-	    SpringApplication.run(RestApplication.class, args);
+		try {
+			System.out.println("Starting: " + getAppStringFromManifest());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SpringApplication.run(RestApplication.class, args);
 	}
 
 }
