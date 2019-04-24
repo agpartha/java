6febd82143c564b2afeb9939984c4b58e39fb97a
Anand Parthasarathy <code.ag@outlook.com>
2019-04-23 23:01:31 -0700

    more api operation details for users endpoint, but ui is not okay

--- simplerest/src/main/java/io/anand/springboot/UserController.java
+++ simplerest/src/main/java/io/anand/springboot/UserController.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,27 +20,67 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
+	@ApiOperation(value = "Get a list of users", response = Iterable.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
 	@RequestMapping("/users")
 	public List<User> getUsers () {
 		return userService.getUsers();
 	}
-	
+
+	@ApiOperation(value = "View a single user information", response = Iterable.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved user information"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
 	@RequestMapping("/users/{name}")
 	public User getUser (@PathVariable String name) {
 		return userService.getUser(name);
 	}
-	
+
+	@ApiOperation(value = "Update a single user information", response = Iterable.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved user information"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
 	@RequestMapping(method=RequestMethod.PUT, value="/users/{name}")
 	public User updUser (@PathVariable String name, @RequestBody User newUser) {
 	    return userService.updUser(name, newUser);
 	}
-	
+
+	@ApiOperation(value = "Create a new user", response = Iterable.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved user information"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
 	@RequestMapping(method=RequestMethod.POST, value="/users/")
 	public boolean addUser (@RequestBody User user) {
 		return userService.addUser(user);
 	}
-	
+
+	@ApiOperation(value = "Delete an user", response = Iterable.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved user information"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
 	@RequestMapping(method=RequestMethod.DELETE, value="/users/{name}")
 	public boolean remUser (@RequestBody User user) {
 	    return userService.remUser(user);
