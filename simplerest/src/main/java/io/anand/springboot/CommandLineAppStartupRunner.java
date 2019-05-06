a39b415749018786b32383beafbd7fe52afc085f
agpartha <code.ag@outlook.com>
2019-05-05 20:01:19 -0700

    Spring profile based development version using H2 and deployment version using mysql, works if spring.profiles.active=deployment is set

--- simplerest/src/main/java/io/anand/springboot/CommandLineAppStartupRunner.java
+++ simplerest/src/main/java/io/anand/springboot/CommandLineAppStartupRunner.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.core.env.Environment;
 
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
@@ -13,11 +14,40 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Autowired
     InfoService infoService;
 
+    @Autowired
+    private Environment environment;
+
+    private void getActiveProfiles() {
+        for (final String profileName : environment.getActiveProfiles()) {
+            logger.info("Currently active profile - " + profileName);
+        }
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        logger.info("\n----Begin logging SimpleRestApplication----");
         logger.info(
                 "Application version: {} . \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t To kill this application, press Ctrl + C.",
                 infoService.getVersion());
-    }
+        logger.info("----System Properties from VM Arguments----");
+        logger.info("server.port: " + System.getProperty("server.port"));
+        logger.info("----Program Arguments----");
+        for (String arg : args) {
+            logger.info(arg);
+        }
 
+        if (environment != null) {
+            getActiveProfiles();
+            logger.info("----Environment Properties----");
+            logger.info("server.port: " + environment.getProperty("server.port"));
+            logger.info("simplerest.environment: " + environment.getProperty("simplerest.environment"));
+            logger.info("spring.datasource.url: " + environment.getProperty("spring.datasource.url"));
+            logger.info("spring.datasource.username: " + environment.getProperty("spring.datasource.username"));
+            logger.info("spring.datasource.password: " + environment.getProperty("spring.datasource.password"));
+            logger.info("spring.jpa.database-platform: " + environment.getProperty("spring.jpa.database-platform"));
+            logger.info("spring.jpa.hibernate.ddl-auto: " + environment.getProperty("spring.jpa.hibernate.ddl-auto"));
+        }
+
+        logger.info("----End logging SimpleRestApplication----");
+    }
 }
