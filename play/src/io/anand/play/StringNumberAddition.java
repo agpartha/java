e4afdf4f2525b97648895379280c6746a8bc8ef1
Anand Girish Parthasarathy <anandpartha@fb.com>
2020-10-22 15:30:14 -0700

    slightly cleaner invcations and usage

--- play/src/io/anand/play/StringNumberAddition.java
+++ play/src/io/anand/play/StringNumberAddition.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class StringNumberAddition {
     // Needed for persisting across invocation
-    private static int carry    = 0;
-    private static int place    = 1;
-    private static int sum      = 0;
+    static int carry    = 0;
+    static int place    = 1;
 
-    private static void addStringDigits (char digOne, char digTwo) {
+    private static int addStringDigits (char digOne, char digTwo, int sum) {
         int val = carry + (digOne - '0') + digTwo - '0';
         carry   = val / 10;
         sum     += (val % 10) * place;
         place   *= 10;
+        return sum;
     }
 
     // Start from the end of both strings and add them one place value
@@ -23,6 +23,7 @@ public class StringNumberAddition {
         // non numeric characters in the input
         // define the expectation and return a defined output to indicate invalid input
 
+        int sum     = 0;
         int len1    = numOne.length();
         int len2    = numTwo.length();
         // Determine the long string and make it as numOne
@@ -45,22 +46,21 @@ public class StringNumberAddition {
 
         // Will traverse both till we finish the shorter string length
         for (int i = len2 - 1; i >= 0; i --)
-            addStringDigits(numOne.charAt(i + diff), numTwo.charAt(i));
+            sum = addStringDigits(numOne.charAt(i + diff), numTwo.charAt(i), sum);
 
         // Remaining digits from longer number
         // not a big one here, but can refactor this logic to reduce duplication and errors
         for (int i = diff - 1; i >= 0; i --)
-            addStringDigits(numOne.charAt(i + diff), '0');
+            sum = addStringDigits(numOne.charAt(i + diff), '0', sum);
 
         // Last addition would have possible left acarry and do not
         // ignore the place value for this carry and add it
-        sum += carry * place;
-        return sum;
+        return addStringDigits('0', '0', sum);
     }
 
     public static void main(String []args){
         String numOne = "9894";
-        String numTwo = "017";
+        String numTwo = "817";
 
         System.out.println("Input: number 1: " + numOne + ", number 2: " + numTwo);
         System.out.println("Sum: " + addStringNumbers(numOne, numTwo));
