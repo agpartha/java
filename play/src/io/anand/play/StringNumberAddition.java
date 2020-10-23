8bd70f58ee1516bd252abbac0aee53be72e16d5d
Anand Girish Parthasarathy <anandpartha@fb.com>
2020-10-22 19:50:13 -0700

    cleaner version

--- play/src/io/anand/play/StringNumberAddition.java
+++ play/src/io/anand/play/StringNumberAddition.java
@@ -3,19 +3,30 @@ package io.anand.play;
 import java.util.Arrays;
 
 public class StringNumberAddition {
-    // Needed for persisting across invocation
-    static int carry    = 0;
-    static int place    = 1;
 
-    private static int addStringDigits (char digOne, char digTwo, int sum) {
-        int val = carry + (digOne - '0') + digTwo - '0';
-        carry   = val / 10;
-        sum     += (val % 10) * place;
-        place   *= 10;
-        return sum;
+    private static class CarryPlace {
+        int carry = 0;
+        int place = 1;
+        int sum   = 0;
+    }
+    private static CarryPlace carryPlace = new CarryPlace();
+
+    private static void addStringDigits (char digOne, char digTwo) {
+        int val             = carryPlace.carry + (digOne - '0') + digTwo - '0';
+        carryPlace.carry    = val / 10;
+        carryPlace.sum     += (val % 10) * carryPlace.place;
+        carryPlace.place   *= 10;
     }
 
-    // Start from the end of both strings and add them one place value
+    private static void addStringDigits (char digOne) {
+        addStringDigits(digOne, '0');
+    }
+
+    private static void addStringDigits () {
+        addStringDigits('0', '0');
+    }
+
+        // Start from the end of both strings and add them one place value
     // at a time, do account for carry
     private static int addStringNumbers (String numOne, String numTwo) {
         // Sanitize the inputs
@@ -23,7 +34,6 @@ public class StringNumberAddition {
         // non numeric characters in the input
         // define the expectation and return a defined output to indicate invalid input
 
-        int sum     = 0;
         int len1    = numOne.length();
         int len2    = numTwo.length();
         // Determine the long string and make it as numOne
@@ -34,7 +44,7 @@ public class StringNumberAddition {
         if (len1 < len2) {
             String temp = numTwo;
             numTwo      = numOne;
-            numTwo      = temp;
+            numOne      = temp;
             int    len  = len2;
             len2        = len1;
             len1        = len;
@@ -45,22 +55,23 @@ public class StringNumberAddition {
         int diff = len1 - len2;
 
         // Will traverse both till we finish the shorter string length
-        for (int i = len2 - 1; i >= 0; i --)
-            sum = addStringDigits(numOne.charAt(i + diff), numTwo.charAt(i), sum);
+        for (int i = len2 - 1; i >= 0; i--)
+            addStringDigits(numOne.charAt(i + diff), numTwo.charAt(i));
 
         // Remaining digits from longer number
         // not a big one here, but can refactor this logic to reduce duplication and errors
-        for (int i = diff - 1; i >= 0; i --)
-            sum = addStringDigits(numOne.charAt(i + diff), '0', sum);
+        for (int i = diff - 1; i >= 0; i--)
+            addStringDigits(numOne.charAt(i));
 
         // Last addition would have possible left acarry and do not
         // ignore the place value for this carry and add it
-        return addStringDigits('0', '0', sum);
+        addStringDigits();
+        return carryPlace.sum;
     }
 
     public static void main(String []args){
-        String numOne = "9894";
-        String numTwo = "817";
+        String numOne = "900097894";
+        String numTwo = "0050087";
 
         System.out.println("Input: number 1: " + numOne + ", number 2: " + numTwo);
         System.out.println("Sum: " + addStringNumbers(numOne, numTwo));
